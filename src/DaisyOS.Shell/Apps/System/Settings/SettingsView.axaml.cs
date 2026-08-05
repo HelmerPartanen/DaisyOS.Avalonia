0ffa0aaf58ca0aefb54240c7091dd86c340e17f0
HelmerPartanen <helmerpartanen3@gmail.com>
2026-08-05 14:33:50 +0300

    Settings app

--- src/DaisyOS.Shell/Apps/System/Settings/SettingsView.axaml.cs
+++ src/DaisyOS.Shell/Apps/System/Settings/SettingsView.axaml.cs
@@ -1,7 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Styling;
 
 namespace DaisyOS.Shell.Apps.System.Settings
 {
@@ -10,30 +7,6 @@ namespace DaisyOS.Shell.Apps.System.Settings
         public SettingsView()
         {
             InitializeComponent();
-        }
-
-        private async void OnLightThemeClicked(object? sender, RoutedEventArgs e)
-        {
-            if (Application.Current is App app)
-            {
-                await app.SetShellThemeAsync(ThemeVariant.Light);
-            }
-        }
-
-        private async void OnDarkThemeClicked(object? sender, RoutedEventArgs e)
-        {
-            if (Application.Current is App app)
-            {
-                await app.SetShellThemeAsync(ThemeVariant.Dark);
-            }
-        }
-
-        private async void OnWallpaperClicked(object? sender, RoutedEventArgs e)
-        {
-            if (sender is Button { Tag: string wallpaperUri } && Application.Current is App app)
-            {
-                await app.SetWallpaperAsync(wallpaperUri);
-            }
         }
     }
 }
