a40fd0b66c3209d4bed57f7cb56fb7f240f1fc4b
HelmerPartanen <helmerpartanen3@gmail.com>
2026-08-06 09:12:59 +0300

    Settings app

--- src/DaisyOS.Shell/Apps/System/Settings/SettingsWindow.axaml.cs
+++ src/DaisyOS.Shell/Apps/System/Settings/SettingsWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace DaisyOS.Shell.Apps.System.Settings;
 
@@ -7,5 +8,53 @@ public partial class SettingsWindow : Window
     public SettingsWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnResizeTopPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.North, e);
+    }
+
+    private void OnResizeBottomPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.South, e);
+    }
+
+    private void OnResizeLeftPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.West, e);
+    }
+
+    private void OnResizeRightPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.East, e);
+    }
+
+    private void OnResizeTopLeftPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.NorthWest, e);
+    }
+
+    private void OnResizeTopRightPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.NorthEast, e);
+    }
+
+    private void OnResizeBottomLeftPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.SouthWest, e);
+    }
+
+    private void OnResizeBottomRightPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginResizeDrag(WindowEdge.SouthEast, e);
     }
 }
