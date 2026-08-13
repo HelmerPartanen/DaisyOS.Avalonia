901b257da943ffe47ab38b1352e0a07386680a81
HelmerPartanen <helmerpartanen3@gmail.com>
2026-08-14 00:26:05 +0300

    Glass

--- src/DaisyOS.Shell/App.axaml.cs
+++ src/DaisyOS.Shell/App.axaml.cs
@@ -20,7 +20,7 @@ namespace DaisyOS.Shell;
 
 public partial class App : Application
 {
-    private const int DefaultKWinBlurStrength = 3;
+    private const int DefaultKWinBlurStrength = 4;
     private IWallpaperService? _wallpaperService;
     private SettingsWindow? _settingsWindow;
     private NotesWindow? _notesWindow;
